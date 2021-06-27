4ff5d2d5e5ebea1846aba5932f46429304c0d8ae
MohamadNezam <mohamad.nezam@mail.polimi.it>
2021-06-27 13:40:11 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-const programmingLanguagesRouter = require('./routes/programmingLanguages');
 
 app.use(bodyParser.json());
 app.use(
@@ -14,18 +13,6 @@ app.use(
 app.get('/', (req, res) => {
   res.json({'message': 'ok'});
 })
-
-app.use('/programming-languages', programmingLanguagesRouter);
-
-/* Error handler middleware */
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-
-
-  return;
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
